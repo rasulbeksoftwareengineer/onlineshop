1eaa970a6340fde5edb57224739cb1a1680bc6ea
Rasulbek Musayev <rasulbeksoftwareengineer@gmail.com>
2025-03-11 16:19:36 +0500

    Authentication

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+// import { createClient } from "@/utils/supabase/server";
+
+export default async function Page() {
+  // const supabase = await createClient();
+  // const { data: productItems } = await supabase.from("products").select("*");
+  // console.log(productItems);
+
+  return <h1></h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/page/footer";
 import Navbar from "@/components/page/navbar";
 import Products from "@/components/page/products";
+import { Toaster } from "@/components/ui/sonner";
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
@@ -11,9 +12,14 @@ export default async function Home() {
     <>
       <Navbar />
 
-      {productItems && <Products items={productItems} />}
+      {(productItems && <Products items={productItems} />) || (
+        <section className="w-full h-[calc(100vh-120px)] flex items-center justify-center">
+          <h1 className="text-3xl">Bizda xatolik ! 404</h1>
+        </section>
+      )}
 
       <Footer />
+      <Toaster />
     </>
   );
 }
